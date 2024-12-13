bf07c79a9bb04ba2a05c897cf51c5515354ea232
Swarthak <swarthakd@gmail.com>
2024-12-14 01:02:13 +0530

    ready to deploy

--- src/app/(app)/employee-dashboard/[employee]/video-conference/[user]/page.tsx
+++ src/app/(app)/employee-dashboard/[employee]/video-conference/[user]/page.tsx
@@ -1,12 +1,13 @@
 import Call from "@/components/Call";
 
-export default function Page({ params }: { params: { user: string } }) {
+export default async function Page({ params }: { params: Promise<{ user: string }> }) {
+    const resolvedParams = await params;
     return (
         <main className="flex w-full flex-col">
             <p className="absolute z-10 mt-2 ml-12 text-2xl font-bold text-gray-900">
-                {params.user!}
+                {resolvedParams.user}
             </p>
-            <Call appId={process.env.PUBLIC_AGORA_APP_ID!} channelName={params.user}></Call>
+            <Call appId={process.env.PUBLIC_AGORA_APP_ID!} channelName={resolvedParams.user}></Call>
         </main>
     )
 }
